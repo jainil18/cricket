991e7b7a6b12b3fce32ba0ca8b2c9b8f7f88f123
jainil18 <gondaliyatushar73@gmail.com>
2024-08-03 16:32:05 +0530

    Upload a website

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var typed = new Typed('#typer', {
+        strings: [' ', 'Some Legend Cricketers Are Here!!'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
